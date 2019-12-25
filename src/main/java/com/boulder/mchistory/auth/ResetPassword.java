3c23ed88d3d9ab56177be3a9c8392b516432e9af
Kat Grennan <caitlyn.grennan@techtonic.com>
2019-12-25 16:44:30 -0700

    Refactor ResetPassword servlet

--- src/main/java/com/boulder/mchistory/auth/ResetPassword.java
+++ src/main/java/com/boulder/mchistory/auth/ResetPassword.java
@@ -1,20 +1,5 @@
 package com.boulder.mchistory.auth;
 
-import java.io.IOException;
-import java.net.URLEncoder;
-import java.sql.SQLException;
-import java.util.Date;
-import java.util.logging.Logger;
-
-import javax.net.ssl.SSLSocketFactory;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.MultipartConfig;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.core.MediaType;
-
 import com.boulder.mchistory.auth.PasswordStorage.CannotPerformOperationException;
 import com.boulder.mchistory.daos.UserDao;
 import com.sun.jersey.api.client.Client;
@@ -23,14 +8,26 @@ import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
 import com.sun.jersey.multipart.FormDataMultiPart;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.logging.Logger;
+
 @SuppressWarnings("serial")
 @MultipartConfig
 @WebServlet(name = "ResetPassword", urlPatterns = {"/ResetPassword"})
 public class ResetPassword extends HttpServlet {
 	
 	private static final Logger logger = Logger.getLogger(ResetPassword.class.getName());
-	SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
-	
+
 	@Override
 	  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
 	      IOException {
@@ -45,6 +42,7 @@ public class ResetPassword extends HttpServlet {
 			e.printStackTrace();
 		}
 		logger.info("Userhash: " + userHash);
+		assert userHash != null;
 		if (userHash.equals(hash)) {
 			req.setAttribute("uid", uid);
 		    req.setAttribute("destination", "ResetPassword");  // The urlPattern to invoke (this Servlet)
@@ -73,7 +71,7 @@ public class ResetPassword extends HttpServlet {
 		case "lost":
 			String email = req.getParameter("email").toLowerCase();
 			try {
-				if (dao.isUser(email) == false) {
+				if (!dao.isUser(email)) {
 					String error = "Email was not found in the database.";
 					req.setAttribute("resetEmail", email);
 					req.setAttribute("resetError", error);
@@ -130,7 +128,7 @@ public class ResetPassword extends HttpServlet {
 		}
 	}
 	
-	public static ClientResponse sendPasswordReset(String email, UserDao dao) throws IOException {
+	private static void sendPasswordReset(String email, UserDao dao) throws IOException {
 		Long uid = null;
 		String rand = new RandomString().toString();
 		String hash = null;
@@ -150,6 +148,7 @@ public class ResetPassword extends HttpServlet {
 		
 		hash = URLEncoder.encode(hash, "UTF-8");
 		Date date = new Date();
+		assert uid != null;
 		String url = "https://www.mchistory.net/ResetPassword?user=" + uid.toString() + "&hash=" + hash;
 		String html = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\"><html xmlns=\"http://www.w3.org/1999/xhtml\" style=\"font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;\"><head><meta name=\"viewport\" content=\"width=device-width\" /><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" /><title>Actionable emails e.g. reset password</title><style type=\"text/css\">img {max-width: 100%;}body {-webkit-font-smoothing: antialiased; -webkit-text-size-adjust: none; width: 100% !important; height: 100%; line-height: 1.6em;}body {background-color: #f6f6f6;}@media only screen and (max-width: 640px) {  body {    padding: 0 !important;  }  h1 {    font-weight: 800 !important; margin: 20px 0 5px !important;  }  h2 {    font-weight: 800 !important; margin: 20px 0 5px !important;  }  h3 {    font-weight: 800 !important; margin: 20px 0 5px !important;  }  h4 {    font-weight: 800 !important; margin: 20px 0 5px !important;  }  h1 {    font-size: 22px !important;  }  h2 {    font-size: 18px !important;  }  h3 {    font-size: 16px !important;  }  .container {    padding: 0 !important; width: 100% !important;  }  .content {    padding: 0 !important;  }  .content-wrap {    padding: 10px !important;  }  .invoice {    width: 100% !important;  }}</style></head><body itemscope itemtype=\"http://schema.org/EmailMessage\" style=\"font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: none; width: 100% !important; height: 100%; line-height: 1.6em; background-color: #f6f6f6; margin: 0;\" bgcolor=\"#f6f6f6\"><table class=\"body-wrap\" style=\"font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; width: 100%; background-color: #f6f6f6; margin: 0;\" bgcolor=\"#f6f6f6\"><tr style=\"font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;\"><td style=\"font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0;\" valign=\"top\"></td>		<td class=\"container\" width=\"600\" style=\"font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; display: block !important; max-width: 600px !important; clear: both !important; margin: 0 auto;\" valign=\"top\">			<div class=\"content\" style=\"font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; max-width: 600px; display: block; margin: 0 auto; padding: 20px;\">				<table class=\"main\" width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" itemprop=\"action\" itemscope itemtype=\"http://schema.org/ConfirmAction\" style=\"font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; border-radius: 3px; background-color: #fff; margin: 0; border: 1px solid #e9e9e9;\" bgcolor=\"#fff\"><tr style=\"font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;\"><td class=\"content-wrap\" style=\"font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 20px;\" valign=\"top\">							<meta itemprop=\"name\" content=\"Reset Password\" style=\"font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;\" /><table width=\"100%\" cellpadding=\"0\" cellspacing=\"0\" style=\"font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;\"><tr style=\"font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;\"><td class=\"content-block\" style=\"font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;\" valign=\"top\">										Click the link below to reset your password.								</td>								</tr><tr style=\"font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;\"><td class=\"content-block\" style=\"font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;\" valign=\"top\">									</td>								</tr><tr style=\"font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;\"><td class=\"content-block\" itemprop=\"handler\" itemscope itemtype=\"http://schema.org/HttpActionHandler\" style=\"font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;\" valign=\"top\">										<a href=\""+url+"\" class=\"btn-primary\" itemprop=\"url\" style=\"font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; color: #FFF; text-decoration: none; line-height: 2em; font-weight: bold; text-align: center; cursor: pointer; display: inline-block; border-radius: 5px; text-transform: capitalize; background-color: #348eda; margin: 0; border-color: #348eda; border-style: solid; border-width: 10px 20px;\">Reset Password</a>									</td>								</tr><tr style=\"font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;\"><td class=\"content-block\" style=\"font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;\" valign=\"top\">										&mdash; MCHS									</td>								</tr></table></td>					</tr></table><div class=\"footer\" style=\"font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; width: 100%; clear: both; color: #999; margin: 0; padding: 20px;\">					<table width=\"100%\" style=\"font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;\"><tr style=\"font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;\"><td class=\"aligncenter content-block\" style=\"font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 12px; vertical-align: top; color: #999; text-align: center; margin: 0; padding: 0 0 20px;\" align=\"center\" valign=\"top\">"+date+" End of message.</td>						</tr></table></div></div>		</td>		<td style=\"font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0;\" valign=\"top\"></td>	</tr></table></body></html>";
 		logger.info("URL: " + url);
@@ -162,9 +161,9 @@ public class ResetPassword extends HttpServlet {
 		  formData.field("to", email);
 		  formData.field("subject", "Reset Your Password");
 		  formData.field("html", html);
-		  
-		  return webResource.type(MediaType.MULTIPART_FORM_DATA_TYPE)
-		      .post(ClientResponse.class, formData);
+
+		webResource.type(MediaType.MULTIPART_FORM_DATA_TYPE)
+				.post(ClientResponse.class, formData);
 	}
 	
 }
